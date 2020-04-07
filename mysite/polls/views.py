2a55b648e2482fba0139fa401ef9732102b32f1f
Elijah <rahulsharma240301@gmail.com>
2020-04-07 10:38:54 +0530

    Added base.html and other templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello world")
+from .models import Question
 
+
+def index(request):
+    """Home page to Display all the questions"""
+    questions_list = Question.objects.all().order_by('-pub_date')
+    context = {'questions_list': questions_list}
+
+    return render(request, 'polls/home.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("Detials")
+
+def results(request, question_id):
+    return HttpResponse("results")
+
+def vote(request, question_id):
+    return HttpResponse("vote")
